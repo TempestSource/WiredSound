afe81ea07a47132b182a3e08f614382a850ddca8
Adam <adstahly@gmail.com>
2026-03-01 10:52:45 -0600

    Now deletes identifier files.

--- lib/watcher/audio_listener.rb
+++ lib/watcher/audio_listener.rb
@@ -9,10 +9,17 @@ class AudioListener
     puts "WiredSound Listener started!"
     puts "Watching for new audio files in: #{watch_directory}"
 
-    listener = Listen.to(watch_directory, only: /\.(mp3|wav|flac|m4a)$/i) do |modified, added, removed|
+    listener = Listen.to(watch_directory) do |modified, added, removed|
       added.each do |file_path|
-        puts "\n New file detected: #{File.basename(file_path)}"
 
+        if file_path.end_with?(':Zone.Identifier')
+          FileUtils.rm(file_path) rescue nil
+          next
+        end
+
+        next unless file_path.match?(/\.(mp3|wav|flac|m4a)$/i)
+
+        puts "\n New file detected: #{File.basename(file_path)}"
         AudioProcessor.call(file_path)
       end
     end
