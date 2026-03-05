6f0da3c6572dd25a04d9cf2cdd3ae056afbc153d
Adam <adstahly@gmail.com>
2026-03-05 10:21:58 -0600

    Updated foreign_keys

--- app/models/album_info.rb
+++ app/models/album_info.rb
@@ -1,7 +1,4 @@
 class AlbumInfo < ApplicationRecord
   self.table_name = "album_info"
   self.primary_key = "albumID"
-
-  belongs_to :artist_info, foreign_key: "artistID"
-  has_many :song_infos, foreign_key: "albumID"
 end
--- app/models/artist_info.rb
+++ app/models/artist_info.rb
@@ -1,7 +1,4 @@
 class ArtistInfo < ApplicationRecord
   self.table_name = "artist_info"
-  self.primary_key = "artistID"
-
-  has_many :song_infos, foreign_key: "artistID"
-  has_many :album_infos, foreign_key: "artistID"
+  self.primary_key = "artirstID"
 end
