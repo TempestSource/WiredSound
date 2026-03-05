d1a8a146b124c9c4cbdec7bc0dd61ec6d333aea1
Adam <adstahly@gmail.com>
2026-03-05 10:18:17 -0600

    Made changes to foreign_key restrictions

--- app/models/song_info.rb
+++ app/models/song_info.rb
@@ -2,9 +2,6 @@ class SongInfo < ApplicationRecord
   self.table_name = "song_info"
   self.primary_key = "songID"
 
-
-  has_many :hash_matches, foreign_key: "songID"
-
-  belongs_to :album_info, foreign_key: "albumID"
-  belongs_to :artist_info, foreign_key: "artistID"
+  belongs_to :artist_info, foreign_key: "artistID", optional: true
+  belongs_to :album_info, foreign_key: "albumID", optional: true
 end
