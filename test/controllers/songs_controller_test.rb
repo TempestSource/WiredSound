09cc5bfecb6ffd408c256936e0f8f182312dde8e
Adam <adstahly@gmail.com>
2026-03-29 18:51:24 -0500

    Updated controller database names

--- test/controllers/songs_controller_test.rb
+++ test/controllers/songs_controller_test.rb
@@ -1,8 +1,10 @@
 require "test_helper"
 
 class SongsControllerTest < ActionDispatch::IntegrationTest
+  set_fixture_class songs: SongInfo
+
   test "should get index" do
-    get songs_index_url
+    get songs_url
     assert_response :success
   end
 end
