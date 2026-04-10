6806baa2c8ca83c6fed31cc78d17732580aa07ee
Adam <adstahly@gmail.com>
2026-04-09 20:57:49 -0500

    Added new route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :artist_infos, only: [:index, :show]
+  resources :album_releases, only: [:index, :show]
   resources :album_artists, only: [:index, :show]
   resources :album_infos, only: [:index, :show]
   resources :hash_matches, only: [:index, :show]
