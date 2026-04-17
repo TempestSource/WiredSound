626771819749ddbb8d111694a968708d8170f342
Lain Iwakura <christopher@macross-2.local>
2026-04-16 22:36:33 -0500

    API Authentication
    Still requires authorization

---
+++ app/controllers/api/v1/auth/auth_controller.rb
@@ -0,0 +1,61 @@
+module Api
+  module V1
+    module Auth
+      class AuthController < ApplicationController
+        skip_before_action :verify_authenticity_token
+
+        # /api/v1/auth/login
+        def login
+          unless params[:username].present? && params[:password].present?
+            return render_error('Requires username and password', :bad_request)
+          end
+
+          user = User.authenticate(params[:username], params[:password])
+
+          unless user
+            return render_error('Invalid username or password', :unauthorized)
+          end
+
+          tokens = JwtAuth.create_token_pair(user.username)
+
+          refresher = TokenRefresh.create_token(username, request.user_agent)
+
+          render json: {
+            access_token: tokens[:access_token],
+            refresh_token: refresher[:token],
+            token_type: 'Bearer',
+            expires_in: ENV.fetch('JWT_ACCESS_TIMEOUT', 600).to_i,
+            user: user_info(user)
+          }, status: :ok
+
+        end
+
+        def sign_up
+          unless params[:username].present? && params[:password].present?
+            return render_error('Requires username and password', :bad_request)
+          end
+
+          User.create!(sign_up_info)
+        end
+
+        # TODO: logout
+        # TODO: refresh
+        private
+
+        def user_info(user)
+          {
+            username: user.username
+          }
+        end
+
+        def sign_up_info
+          params.permit(:username, :password)
+        end
+
+        def render_error(message, status)
+          render json: { error: message }, status: status
+        end
+      end
+    end
+  end
+end
---
+++ db/migrate/20260416001544_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[8.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest, null: false
+      t.string :role
+
+      t.timestamps
+    end
+
+    add_index :users, :username, unique: true
+  end
+end
