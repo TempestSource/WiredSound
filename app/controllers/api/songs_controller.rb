e56d54861113fa4cf85ec105b1bc76eae8a0dae1
Lain Iwakura <christopher@macross-2.local>
2026-04-20 00:28:45 -0500

    Finish API endpoint for /songs
    Also cleaned up old endpoint controllers

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,6 +1,7 @@
 module Api
   class SongsController < ApiController
     skip_before_action :authenticate, only: [ :index, :show ]
+    before_action :admin_page, only: [:destroy, :update ]
 
     def index
       songs = SongInfo.all
@@ -16,6 +17,32 @@ module Api
       }
     rescue ActiveRecord::RecordNotFound
       render_error("Song not found", 404)
+    end
+
+    def destroy
+      song = SongInfo.find(params[:id])
+      artists = SongArtist.where(songID: song[:songID])
+      song.destroy
+      artists.each do |a|
+        a.destroy
+      end
+    end
+
+    def update
+      song = SongInfo.find(params[:id])
+      if song.update(song_params)
+        render json: song, status: :ok
+      else
+        render_error(song.errors, :unprocessable_entity)
+      end
+    rescue ActiveRecord::RecordNotFound
+      render_error("Song not found", 404)
+    end
+
+    private
+
+    def song_params
+      params.permit(:songName, :trackNumber, :releaseID)
     end
 
   end
