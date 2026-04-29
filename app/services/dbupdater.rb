4e98d0bb06d6b2fc891dafa98b459baca924fefe
Adam <adstahly@gmail.com>
2026-04-28 23:19:04 -0500

    Ui now working

--- app/services/dbupdater.rb
+++ app/services/dbupdater.rb
@@ -42,10 +42,9 @@ class Dbupdater
   def add_album_artists(data)
     data.each do |artist|
       AlbumArtist.find_or_create_by!(
+        albumID: artist[0] || '',
         artistID: artist[1] || ''
-      ) do |a|
-        a.albumID = AlbumInfo.find_by_albumID(artist[0]).albumID || ''
-      end
+      )
     end
   end
 
@@ -71,10 +70,9 @@ class Dbupdater
   def add_song_artists(data)
     data.each do |artist|
       SongArtist.find_or_create_by!(
+        songID: artist[0] || '',
         artistID: artist[1] || ''
-      ) do |a|
-        a.songID = SongInfo.find_by_songID(artist[0]).songID || ''
-      end
+      )
     end
   end
 
